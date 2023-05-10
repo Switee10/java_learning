935d1e7f9f77024f6b738019d159007e2795559e
sweety <switeesonawane10@gmail.com>
2023-05-10 22:31:41 +0530

    helloworld source code

---
+++ src/Basics/HelloWorld1.java
@@ -0,0 +1,12 @@
+package Basics;
+
+public class HelloWorld1 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello world1");
+		// TODO Auto-generated method stub
+
+	}
+
+}
