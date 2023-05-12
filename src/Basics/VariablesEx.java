07e22c3c8f7acea5cbba8d29232ed9cef4978760
sweety <switeesonawane10@gmail.com>
2023-05-12 12:40:12 +0530

    variables & datatypes

---
+++ src/Basics/VariablesEx.java
@@ -0,0 +1,27 @@
+package Basics;
+
+public class VariablesEx {
+
+	public static void main(String[] args)
+	{
+		int ticketPrize;
+		double pi=3.14;
+		short Srno=1;
+		char gender ='F';
+	    String name = "Sweety";
+				
+				
+		ticketPrize = 9000;
+		
+		
+		
+		System.out.println("the ticket prize is "+ticketPrize);
+		System.out.println("mathematical pi value is"+pi);
+		System.out.println("gender is "+gender);
+		System.out.println("name is " +name);
+		
+		
+
+	}
+
+}
