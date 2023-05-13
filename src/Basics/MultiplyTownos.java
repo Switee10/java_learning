353c06fe81157d419c100a165c86839c64704b1e
sweety <switeesonawane10@gmail.com>
2023-05-13 15:06:43 +0530

    multiplcation

---
+++ src/Basics/MultiplyTownos.java
@@ -0,0 +1,16 @@
+package Basics;
+
+public class MultiplyTownos {
+
+	public static void main(String[] args) 
+	{
+		int a=10;
+		int b=10;
+		int c;
+		c=a*b;
+		System.out.println("Multiplication is "+c);
+		
+
+	}
+
+}
