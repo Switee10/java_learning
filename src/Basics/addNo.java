b6ccedc63c6231864b85cf30c3c4c6f04de2320e
sweety <switeesonawane10@gmail.com>
2023-05-13 14:00:24 +0530

    add two numbers

---
+++ src/Basics/addNo.java
@@ -0,0 +1,17 @@
+package Basics;
+
+public class addNo {
+
+	public static void main(String[] args)
+	{
+		int a=10;
+		int b=10;
+		int c;
+		c=a+b;
+		
+		System.out.println("Addition is "+c);
+		
+
+	}
+
+}
