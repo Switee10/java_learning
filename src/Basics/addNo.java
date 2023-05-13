adf86484796d33fd5983bdbdcb6c5906eb890468
sweety <switeesonawane10@gmail.com>
2023-05-13 14:48:38 +0530

    Add numbers

--- src/Basics/addNo.java
+++ src/Basics/addNo.java
@@ -1,16 +1,16 @@
 package Basics;
 
-public class addNo {
+public class AddNo {
 
-	public static void main(String[] args)
+	public static void main(String[] args) 
 	{
 		int a=10;
-		int b=10;
+		int b=20;
 		int c;
 		c=a+b;
-		
-		System.out.println("Addition is "+c);
-		
+			System.out.println("Addition is"+c);
+
+	
 
 	}
 
